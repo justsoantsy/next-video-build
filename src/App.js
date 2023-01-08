c1522a29e0563683ed58aa028f2b83cc73e02477
salti <justsoantsy@gmail.com>
2023-01-08 03:12:14 -0700

    Livepeer and description

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import {   LivepeerConfig, createReactClient, studioProvider, Player } from '@livepeer/react';
+
+const client = createReactClient({
+  provider: studioProvider({ apiKey: "a4947a51-7a2a-42c1-80d9-38471741c5f1" }),
+});
 
 function App() {
   return (
     <div>
       <Navbar />
+      <LivepeerConfig client={client}>
+      <Player
+      title="Next Video Build"
+      playbackId="6c410ysidtuajn2cf"
+      showPipButton
+      objectFit="cover"
+    />
+      </LivepeerConfig>
     </div>
   );
 }
