a2c545a14d14fcecff419185fead94ff73be9f32
salti <justsoantsy@gmail.com>
2023-01-08 05:07:55 -0700

    Updated player with brand colors

--- src/App.js
+++ src/App.js
@@ -7,16 +7,37 @@ const client = createReactClient({
   provider: studioProvider({ apiKey: "a4947a51-7a2a-42c1-80d9-38471741c5f1" }),
 });
 
+const theme = {
+  borderStyles: {
+    containerBorderStyle: 'hidden',
+  },
+  colors: {
+    accent: '#E748E6',
+  },
+  fonts: {
+    display: 'Syne',
+  },
+  space: {
+    controlsBottomMarginX: '10px',
+    controlsBottomMarginY: '5px',
+    controlsTopMarginX: '15px',
+    controlsTopMarginY: '10px',
+  },
+  radii: {
+    slider: '4px',
+  },
+};
+
 function App() {
   return (
     <div>
       <Navbar />
-      <LivepeerConfig client={client}>
+      <LivepeerConfig client={client} theme={theme}>
       <Player
       title="Next Video Build"
       playbackId="c410ysidtuajn2cf"
       showPipButton
-      objectFit="cover"
+      objectFit="contain"
     />
       </LivepeerConfig>
     </div>
