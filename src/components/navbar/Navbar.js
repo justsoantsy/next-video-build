98c066b3ab4b15795423e8d794e90320512c4b83
salti <justsoantsy@gmail.com>
2023-01-08 02:04:42 -0700

    Initialize project using Create React App

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react'
+import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
+import './Navbar.css'
+import Logo from '../../assets/dragverselogo.png'
+
+const Navbar = () => {
+    const [click, setClick] = useState(false)
+    const handleClick = () => setClick(!click)
+    return (
+        <div className='navbar'>
+            <div className="container">
+            <img src={Logo} alt=""/>
+                <ul className={click ? 'nav active' : 'nav'}>
+                <li className="nav-item">
+                        <a href="https://www.figma.com/proto/yvGJs02e7EpemqoXtPpJNA/Dragverse-V3?node-id=108%3A958&scaling=scale-down&page-id=108%3A570&starting-point-node-id=108%3A958/">About Us</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="https://dragverse.io/">Website</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="https://github.com/justsoantsy/dragverse.git/">Git</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="https://twitter.com/metadragverse/">Twitter</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="/">Prototypes</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className='btn' href="https://social.reinasalti.xyz/dragverse/">VISIT DRAGVERSE</a>
+                    </li>
+                </ul>
+                <div onClick={handleClick} className="hamburger">
+                    {click ? (<AiOutlineClose className='icon' />) : (<AiOutlineMenu className='icon' />)}
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
