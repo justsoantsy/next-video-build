1bd724739d6542d84e8e3100ea72dbcdbd383585
salti <justsoantsy@gmail.com>
2023-01-09 22:24:20 -0700

    Installed Lense, React Native and Profile feed

--- src/routes/Feed.js
+++ src/routes/Feed.js
@@ -1,93 +1,59 @@
-import React from 'react'
-import styles from './Feed.css'
+import { useEffect, useState } from 'react'
+import { client, exploreProfiles } from '../api'
 import Navbar from '../components/navbar/Navbar';
 import Footer from '../components/footer/Footer';
 
-import Post from '../components/feed/Posts'
+export default function Home() {
+  /* create initial state to hold array of profiles */
+  const [profiles, setProfiles] = useState([])
+  useEffect(() => {
+    fetchProfiles()
+  }, [])
+  async function fetchProfiles() {
+    try {
+      /* fetch profiles from Lens API */
+      let response = await client.query({ query: exploreProfiles })
+      /* loop over profiles, create properly formatted ipfs image links */
+      let profileData = await Promise.all(response.data.exploreProfiles.items.map(async profileInfo => {
+        let profile = { ...profileInfo }
+        let picture = profile.picture
+        if (picture && picture.original && picture.original.url) {
+          if (picture.original.url.startsWith('ipfs://')) {
+            let result = picture.original.url.substring(7, picture.original.url.length)
+            profile.avatarUrl = `http://lens.infura-ipfs.io/ipfs/${result}`
+          } else {
+            profile.avatarUrl = picture.original.url
+          }
+        }
+        return profile
+      }))
 
-
-import Profile from '../assets/feed/profile.png'
-import Write from '../assets/feed/Write.png'
-import Burger from '../assets/feed/Burger.png'
-import Star from '../assets/feed/Star.svg'
-// import Settings from '../assets/feed/settings.png'
-// import BetaCup from '../assets/feed/BetaCup.png'
-import Settings from '../assets/feed/Slider.svg'
-import BetaCup from '../assets/feed/bottle.svg'
-import ArrowRight from '../assets/feed/Arrow right.svg'
-import Stream from '../assets/feed/streampurple.svg'
-import Location from '../assets/feed/Location.svg'
-import CreatePost from '../components/feed/CreatePost'
-import { useState } from 'react'
-
-export default function Feed() {
-    const [show, setShow] = useState(false)
-    
-    const handleShow = () => {
-        setShow(!show)
+      /* update the local state with the profiles array */
+      setProfiles(profileData)
+    } catch (err) {
+      console.log({ err })
     }
-    return (
-    <div className={styles.build}>
-        <Navbar />
-            <div className={styles.contain}>
-                <div className={styles.container}>
-                    <div className={styles.container1}>
-                        <div className={styles.search}>
-                            <div className={styles.imgIn}>
-                                <img src={Profile} alt='profile' width={40} height={40} />
-                                <input type='create-post' placeholder="What’s the tea?" className={styles.input} />
-                            </div>
-                            <img src={Write} alt='Edit' width={23} height={23} onClick={handleShow}/>
-                        </div>
-                        {show ?   <CreatePost /> : null}
-                        <div className={styles.widget}>
-                            <div className={styles.widget1}>
-                                <button className={styles.button1}>
-                                    <img src={Burger} alt='hamburger' height={25} width={25} />
-                                    <h3>Timeline</h3>
-                                </button>
-                                <button className={styles.button2}>
-                                    <img src={Star} alt='star' height={23} width={23} />
-                                    <h3>Highlights</h3>
-                                </button>
-                            </div>
-                            <img src={Settings} alt='settings' height={25} width={25} />
-                        </div>
-                        <Post />
-                    </div>
-                    <div className={styles.container2}>
-                        <div className={styles.card1}>
-                            <h2>
-                                <img src={BetaCup} alt='' width={20} height={25} className={styles.streamImg}/>
-                                Beta warning!</h2>
-                            <p>The dragverse is still in the beta phase, things may break, please be patience as we build the best drag social network.</p>
-                        </div>
-
-                        <h3>START HERE</h3>
-
-                        <div className={styles.card2}>
-                            <h2>
-                                <img src={Stream} alt='' width={30} height={20} />
-                                GO LIVE</h2>
-                            <p>Ready to share your drag magic with the world? Star livestreaming now! 💄</p>
-                            <button>GO LIVE
-                                <img src={ArrowRight} alt='' height={24} width={24}/>
-                            </button>
-                        </div>
-
-                        <div className={styles.card3}>
-                            <h2>
-                                <img src={Location} alt='' width={30} height={20} />
-                                VISIT DRAGVERSE</h2>
-                            <button>EXPLORE OUR SPATIAL SPACE
-                                <img src={ArrowRight} alt='' height={24} width={24}/>
-                            </button>
-                        </div>
-                    </div>
-                </div>
+  }
+  return (
+    <div className='pt-20'>
+                <Navbar />
+      <div className='flex flex-col justify-center items-center'>
+        <h1>Hello Lens 🌿</h1>
+        {
+          profiles.map(profile => (
+            <div key={profile.id} className='w-2/3 shadow-md p-6 rounded-lg mb-8 flex flex-col items-center'>
+              <img className='w-48' src={profile.avatarUrl || 'https://picsum.photos/200'} />
+              <p className='text-xl text-center mt-6'>{profile.name}</p>
+              <p className='text-base text-gray-400  text-center mt-2'>{profile.bio}</p>
+              <a href={`/profile/${profile.handle}`}>
+                <p className='cursor-pointer text-violet-600 text-lg font-medium text-center mt-2 mb-2'>{profile.handle}</p>
+              </a>
+              <p className='text-pink-600 text-sm font-medium text-center'>{profile.stats.totalFollowers} followers</p>
             </div>
-            <Footer />           
-        </div>
-
+          ))
+        }
+      </div>
+      <Footer />
+    </div>
   )
 }
